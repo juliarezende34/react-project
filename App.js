f99828b21ba1866d65aabc204494bcfe0c903e4f
juliarezende34 <2019952135@teiacoltec.org>
2021-08-04 11:10:55 -0300

    Trabalhando com CSS

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View } from 'react-native';
 import Page from './views/page';
+import {css} from './assets/css/Css';
 
 export default function App() {
   const props={
@@ -10,18 +11,11 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
+    <View style = {css.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Page {...props}/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ assets/css/Css.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const css = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    textPage:{
+        backgroundColor:'#add8e6',
+        padding: 20
+    }
+  });
+
+  export {css};
